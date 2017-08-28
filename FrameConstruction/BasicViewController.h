99f5b486b5034a33860df95e81079769ee7eebd7
BillBo <496081649@qq.com>
2017-08-28 14:32:12 +0800

    扫码功能

---
+++ FrameConstruction/BasicViewController.h
@@ -0,0 +1,13 @@
+//
+//  BasicViewController.h
+//  FrameConstruction
+//
+//  Created by BillBo on 2017/8/28.
+//  Copyright © 2017年 BillBo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BasicViewController : UIViewController
+
+@end
--- FrameConstruction/ViewController.m
+++ FrameConstruction/ViewController.m
@@ -7,18 +7,51 @@
 //
 
 #import "ViewController.h"
+#import "ZHScanView.h"
+@interface ViewController (){
+    
+    ZHScanView *scanView;
 
-@interface ViewController ()
+}
 
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
+    
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+
+    scanView = [[ZHScanView alloc] initWithFrame:CGRectMake(0, 0, self.view.frame.size.width , self.view.frame.size.height)];
+    
+
+    scanView.promptMessage = @"请扫描二维码";
+    
+    [scanView startScaning];
+    
+    [scanView outPutResult:^(NSString *result) {
+        
+        NSLog(@"扫描结果 result %@", result);
+        
+    }];
+    
+    [self.view addSubview:scanView];
+    
 }
 
+- (void)viewDidLayoutSubviews{
+    
+    [super viewDidLayoutSubviews];
+    
+}
+
+- (void)viewWillAppear:(BOOL)animated {
+    
+    [super viewWillAppear:animated];
+    
+    [scanView scanAgain];
+    
+}
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
